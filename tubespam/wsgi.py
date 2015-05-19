9f493e21684c41b38dd178da8a5c62f3480dea56
Tulio Casagrande Alberto <tuliocasagrande@gmail.com>
2015-05-19 15:48:00 -0300

    Updated to Django 1.8

--- tubespam/wsgi.py
+++ tubespam/wsgi.py
@@ -4,11 +4,11 @@ WSGI config for tubespam project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
-from dj_static import Cling
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ["DJANGO_SETTINGS_MODULE"] = "tubespam.settings"
@@ -17,5 +17,4 @@ def application(environ, start_response):
     for key in environ:
         if key.startswith('TS_'):
             os.environ[key] = environ[key]
-    return Cling(get_wsgi_application())(environ, start_response)
-    #return get_wsgi_application()(environ, start_response)
+    return get_wsgi_application()(environ, start_response)
