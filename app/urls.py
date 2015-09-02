842576311badb81fb57f364d1644e7f3003fe822
Tulio Casagrande Alberto <tuliocasagrande@gmail.com>
2015-09-02 15:24:42 -0300

    Revamp /about

--- app/urls.py
+++ app/urls.py
@@ -11,9 +11,6 @@ urlpatterns = patterns('',
   url(r'^spam$', views.spam, name='spam'),
   url(r'^predict$', views.predict, name='predict'),
 
-  # ex: /classify?v=12345678901
-  url(r'^classify$', views.classify, name='classify'),
-
   url(r'^saveComment$', views.saveComment, name='saveComment'),
   url(r'^train$', views.train, name='train'),
   url(r'^export$', views.export, name='export'),
