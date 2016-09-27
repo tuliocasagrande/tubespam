2a60fed9e341cde7921762cd938efcda1deba367
Tulio Casagrande <tcasagra@thoughtworks.com>
2016-09-27 18:58:44 -0300

    Set default of commentCount to zero
    When the video has disabled comments, the youtube api isn't returning
    the commentCount key, which was resulting in a KeyError

--- app/youtube_api.py
+++ app/youtube_api.py
@@ -4,8 +4,8 @@ from apiclient.errors import HttpError
 from datetime import datetime
 
 DEVELOPER_KEY = os.environ['TS_YOUTUBE_API_KEY']
-YOUTUBE_API_SERVICE_NAME = "youtube"
-YOUTUBE_API_VERSION = "v3"
+YOUTUBE_API_SERVICE_NAME = 'youtube'
+YOUTUBE_API_VERSION = 'v3'
 DATE_FORMAT_YT_API = '%Y-%m-%dT%H:%M:%S.%fZ'
 
 # Call the videos.list method to retrieve details for the given video_id.
@@ -24,18 +24,23 @@ def get_video_by_id(video_id):
       search_result = search_response['items'][0]
       video_details['video_id'] = video_id
       video_details['player'] = search_result['player']['embedHtml']
-      video_details['title'] = search_result['snippet']['title']
-      video_details['channel_id'] = search_result['snippet']['channelId']
-      video_details['description'] = search_result['snippet']['description']
-      video_details['channelTitle'] = search_result['snippet']['channelTitle']
-      video_details['publishedAt'] = datetime.strptime(
-        search_result['snippet']['publishedAt'], DATE_FORMAT_YT_API)
-      video_details['viewCount'] = search_result['statistics']['viewCount']
-      video_details['commentCount'] = search_result['statistics']['commentCount']
+
+      _snippet = search_result['snippet']
+      video_details['title'] = _snippet['title']
+      video_details['channel_id'] = _snippet['channelId']
+      video_details['description'] = _snippet['description']
+      video_details['channelTitle'] = _snippet['channelTitle']
+
+      _publishedAt = datetime.strptime(_snippet['publishedAt'], DATE_FORMAT_YT_API)
+      video_details['publishedAt'] = _publishedAt
+
+      _statistics = search_result['statistics']
+      video_details['viewCount'] = _statistics.get('viewCount', 0)
+      video_details['commentCount'] = _statistics.get('commentCount', 0)
     return video_details
 
   except HttpError, e:
-    print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+    print 'An HTTP error %d occurred:\n%s' % (e.resp.status, e.content)
 
 
 # Call the videos.list method to retrieve details for each video.
@@ -50,16 +55,21 @@ def get_videos_by_params(params):
     for search_result in search_response.get('items', []):
       video_details = {}
       video_details['video_id'] = search_result['id']
-      video_details['title'] = search_result['snippet']['title']
-      video_details['channelTitle'] =  search_result['snippet']['channelTitle']
-      video_details['thumbnail'] =  search_result['snippet']['thumbnails']['medium']['url']
-      video_details['viewCount'] =  search_result['statistics']['viewCount']
-      video_details['commentCount'] =  search_result['statistics']['commentCount']
+
+      _snippet = search_result['snippet']
+      video_details['title'] = _snippet['title']
+      video_details['channelTitle'] = _snippet['channelTitle']
+      video_details['thumbnail'] = _snippet['thumbnails']['medium']['url']
+
+      _statistics = search_result['statistics']
+      video_details['viewCount'] = _statistics.get('viewCount', 0)
+      video_details['commentCount'] = _statistics.get('commentCount', 0)
+
       video_details_list.append(video_details)
     return video_details_list
 
   except HttpError, e:
-    print "An HTTP error %d occurred:\n%s" % (e.resp.status, e.content)
+    print 'An HTTP error %d occurred:\n%s' % (e.resp.status, e.content)
 
 # Call the commentThreads.list method to retrieve comments of the given video_id.
 def get_comment_threads(video_id, next_page_token=None):
@@ -78,11 +88,13 @@ def get_comment_threads(video_id, next_page_token=None):
   for search_result in search_response.get('items', []):
     comment = {}
     comment['id'] = search_result['id']
-    top_level_comment = search_result["snippet"]["topLevelComment"]
-    comment['author'] = top_level_comment["snippet"]["authorDisplayName"]
-    comment['content'] = top_level_comment["snippet"]["textDisplay"]
-    comment['date'] = datetime.strptime(
-      top_level_comment['snippet']['publishedAt'], DATE_FORMAT_YT_API)
-    comment_list.append(comment)
 
+    _snippet = search_result['snippet']['topLevelComment']['snippet']
+    comment['author'] = _snippet['authorDisplayName']
+    comment['content'] = _snippet['textDisplay']
+
+    _publishedAt = datetime.strptime(_snippet['publishedAt'], DATE_FORMAT_YT_API)
+    comment['date'] = _publishedAt
+
+    comment_list.append(comment)
   return comment_list, search_response.get('nextPageToken', False)
